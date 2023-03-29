c34fa999eba52f6e29eac3ebeb1d13079129d2db
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-29 05:16:07 -0700

    addlib

--- main.h
+++ main.h
@@ -3,6 +3,10 @@
 
 #include<stdio.h>
 #include<stdarg.h>
+#include<stdlib.h>
+#include<string.h>
+#include<unistd.h>
+
 
 void print_numbers(const char *separator, const unsigned int n, ...);	
 void print_strings(const char *separator, const unsigned int n, ...);
