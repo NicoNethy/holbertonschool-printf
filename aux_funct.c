ffdc2fe38580e9a255e9bb02a6739a455d42c0d0
NahuelSilva28 <nahuelsilvacarugo96@gmail.com>
2023-03-30 11:49:40 -0700

    aux

--- aux_funct.c
+++ aux_funct.c
@@ -8,18 +8,7 @@
 
 int printChar(va_list args)
 {
-	char *str = va_arg(args, char *);
-	(void) mode;
-
-	if (!str)
-	{
-		write(1, "(null)", 6);
-		return (6);
-	}
-
-	write(1, str, strlen(str));
-
-	return (strlen(str));
+	return (_putchar(va_arg(args, int)));
 }
 
 /**
@@ -42,7 +31,6 @@ int printString(va_list args)
 int printNum(va_list args)
 {
 	long int n = va_arg(args, int);
-	int isNegative = 0;
 
 	if (n < 0)
 	{
@@ -80,58 +68,4 @@ int putcharFunc(va_list args, int mode)
 	write(1, &c, 1);
 
 	return (1);
-}
-/**
- * printInt - prints an integer
- * @args: va_list containing an integer
- * @base: base to use for conversion
- *
- * Return: number of characters printed
- */
-int printInt(va_list args, int base)
-{
-	char *buffer;
-	long int n = va_arg(args, long int);
-	int i = 0, len, negative = 0;
-
-	if (n < 0)
-	{
-		negative = 1;
-		n = -n;
-	}
-
-	buffer = malloc(32);
-	if (!buffer)
-		return (0);
-
-	if (n != 0)
-	{
-		while (n > 0)
-		{
-			if (base <= 10)
-				buffer[i++] = ((n % base) + '0');
-			else if (base == 16)
-			{
-				if ((n % base) >= 10)
-					buffer[i++] = ((n % base) + 87);
-				else
-					buffer[i++] = ((n % base) + '0');
-			}
-			n = n / base;
-		}
-		if (negative && base == 10)
-			buffer[i++] = '-';
-		for (int j = i - 1; j >= 0; j--)
-			write(1, &buffer[j], 1);
-	}
-	else
-	{
-		buffer[i++] = '0';
-		write(1, &buffer[i - 1], 1);
-	}
-
-	len = strlen(buffer);
-	free(buffer);
-
-return (len);
 }
